4359d965d95b454cbee0e0aff30566607589c4c5
Amur Bashirov <bashirov@byu.edu>
2025-02-23 20:30:39 -0700

    started on heap implemenationa and initialized classes

--- Data_Structures.py
+++ Data_Structures.py
@@ -12,3 +12,30 @@ def pop_min(H: dict[int, float]) -> tuple[int, float]:
     del H[node]
 
     return node
+
+
+class PriorityHeap:
+    def __init__(self):
+        self.H = []
+
+
+
+    def Insert(self, node: int, distance: float):
+        new_tuple = (node,distance)
+        self.H.append((node,distance))
+        self.bubble_up(node, distance)
+
+
+
+    def bubble_up(self, node: int, distance: float):
+        pass
+
+
+    def bubble_down(self, node: int, distance: float):
+        pass
+
+
+
+
+    def delete_min(self) -> tuple[int, float]:
+        pass
--- network_routing.py
+++ network_routing.py
@@ -1,5 +1,5 @@
 import math
-from Data_Structures import pop_min
+from Data_Structures import pop_min, PriorityHeap
 
 
 def find_shortest_path_with_heap(
@@ -15,6 +15,41 @@ def find_shortest_path_with_heap(
         - the list of nodes (including `source` and `target`)
         - the cost of the path
     """
+    print(
+        f"find_shortest_path_with_array({graph}, {source}, {target})")  # TODO: remove this line after you implement this functionsource, target)
+    nodes = set()
+    for parent, edges in graph.items():
+        nodes.add(parent)
+        nodes.update(edges.keys())
+    print(f"nodes = {nodes}")
+    dist = {}
+    prev = {}
+    for node in nodes:
+        dist[node] = math.inf
+        prev[node] = None
+
+    dist[source] = 0
+    H = PriorityHeap()
+    H.Insert(source, 0)
+
+    while H:
+        u = H.delete_min()
+        parent = u[0]
+        for v, w in graph[parent].items():
+            if dist[v] > dist[u] + w:
+                dist[v] = dist[u] + w
+                prev[v] = u
+                H.Insert(v, dist[v])
+
+    path = [target]
+    while target != source:
+        target = prev[target]
+        path.append(target)
+
+    path.reverse()
+    return path, dist[target]
+
+
 
 
 
@@ -49,19 +84,23 @@ def find_shortest_path_with_array(
     H = {source: 0}
     while H:
         u = pop_min(H)
-        for v, w in graph[u].items():
+        for v, w in graph[u[0]].items():
             if dist[u] + w < dist[v]:
                 dist[v] = dist[u] + w
                 prev[v] = u
                 H[v] = dist[v]
 
+
+
     total_weight = dist[target]
-    path = {}
+    path = []
     current = target
     while current != source:
         current = prev[current]
         path.append(current)
     path.append(source)
+    path.reverse()
+
 
 
     return path, total_weight
